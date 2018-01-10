d69f0bcae1dbaa9402db59cef20d948c4d32e032
JoaoSant0s <ricardoelric@gmail.com>
2018-01-09 23:16:19 -0300

    Creating two player. Each player have you controllers and color Updating shot position. Using joystick as controller

--- scripts/Ship.gd
+++ scripts/Ship.gd
@@ -1,16 +1,20 @@
 extends Node2D
 
+export var characterNumber = 0
+export var velocity = 200
+export (Color) var color
 var prefabShot = preload("res://scenes/Shot.tscn")
-var shooting = false
 var interval = 0.3
 var lastShot = 0
-export var velocity = 200
 var sprite
+var cannonOrigin
 var playerLimits = OS.get_window_size()
 
 func _ready():
 	set_process(true)
 	sprite = get_node("sprite")
+	cannonOrigin = get_node("cannonOrigin")
+	sprite.set_modulate(color)
 	pass
 
 func _process(delta):
@@ -21,18 +25,18 @@ func _process(delta):
 	pass
 
 
-func _check_shoot(delta):	 
-	if Input.is_action_pressed("control_shoot"):
+func _check_shoot(delta):
+	if Input.is_action_pressed("control_shoot_" + str(characterNumber)):
 		if lastShot <= 0:
 			var instance = prefabShot.instance()
-			instance.set_global_pos(get_global_pos())
-			get_node("../").add_child(instance)	
-			lastShot = interval		
-	pass 
-	
+			instance.set_global_pos(cannonOrigin.get_global_pos())
+			get_node("../").add_child(instance)
+			lastShot = interval
+	pass
+
 	if lastShot > 0:
 		lastShot -=delta
-	
+
 func _set_player_limits():
 	var spriteSize = sprite.get_texture().get_size()
 	var positionX = min( max(get_pos().x, spriteSize.x/2), (playerLimits.x - spriteSize.x/2) )
@@ -42,27 +46,25 @@ func _set_player_limits():
 	pass
 
 func _set_vertical_controllers(delta):
-	var up = 0
-	var down = 0
+	var direction = 0	
 
-	if Input.is_action_pressed("control_up"):
-		up = -1
+	if Input.is_action_pressed("control_up_" + str(characterNumber)):
+		direction = -1
 
-	if Input.is_action_pressed("control_down"):
-		down = 1
+	if Input.is_action_pressed("control_down_" + str(characterNumber)):
+		direction = 1
 
-	set_pos(get_pos() + Vector2(0, 1) * delta * velocity * (up + down))
+	set_pos(get_pos() + Vector2(0, 1) * delta * velocity * direction)
 	pass
 
 func _set_horizontal_controllers(delta):
-	var left = 0
-	var right = 0
+	var direction = 0	
 
-	if Input.is_action_pressed("control_right"):
-		right = 1
+	if Input.is_action_pressed("control_right_" + str(characterNumber)):
+		direction = 1
 
-	if Input.is_action_pressed("control_left"):
-		left = -1
+	if Input.is_action_pressed("control_left_" + str(characterNumber)):
+		direction = -1
 
-	set_pos(get_pos() + Vector2(1, 0) * delta * velocity * (right + left))
+	set_pos(get_pos() + Vector2(1, 0) * delta * velocity * direction)
 	pass
